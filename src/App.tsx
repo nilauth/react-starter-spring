6c6e86a42079cc21ef1066ad151ba2aeb355ad8d
0nizzr <nizzrdev@gmail.com>
2024-03-08 18:21:46 +0100

    react with vite + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className='h-screen flex justify-center items-center'>
+      <h1 className='text-2xl text-blue-600'>React + Spring Boot starter</h1>
+    </main>
+  );
+};
+
+export default App;
